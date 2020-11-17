b47251291eb389497d01ac5ab327a2739298f75f
Fony-Stark <55931556+Fony-Stark@users.noreply.github.com>
2020-11-17 14:54:57 +0100

    Just a minor edit

--- brain_to_control_hue.js
+++ brain_to_control_hue.js
@@ -13,7 +13,7 @@ async function get_all_lights(){
     }
 }
 
-let standard = "https://192.168.1.116/api/9VVcN-PWsDxs-bmWgNPOE0N4SfzO-OTdFypVWEO9";
+let standard = "https://192.168.1.116/api/";
 function turn_off_light(light, index){
     let xhr = new XMLHttpRequest();
     let encoded_url = encodeURI(standard + "/lights/" + index + "/state");
