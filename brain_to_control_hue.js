7a6b1ba01dcd1ad9fe73e404f083c553bc3a7fc3
Karl Holt <kholt19@student.aau.dk>
2020-11-22 20:56:00 +0100

    Made the new user interface. Added colour.

--- brain_to_control_hue.js
+++ brain_to_control_hue.js
@@ -1,6 +1,6 @@
-let standard = "https://192.168.1.116/api/";
+let standard = "https://192.168.1.116/api/9VVcN-PWsDxs-bmWgNPOE0N4SfzO-OTdFypVWEO9";
 async function get_all_lights(){
-    let response = await fetch("https://192.168.1.116/api/9VVcN-PWsDxs-bmWgNPOE0N4SfzO-OTdFypVWEO9/lights");
+    let response = await fetch(standard + "/lights");
     if(response.ok){
         let light_object = await response.json();
         //console.log(light_object);
@@ -43,12 +43,30 @@ reloadLights();
 
 document.getElementById("ReLoad").addEventListener("click", reloadLights);
 
-function create_light_mode(light, index){
-    let container = document.createElement("div");
-    container.classList.add("containers");
+function remove_specific(){
+    document.getElementById("back_click_killer_window").removeEventListener("click", remove_specific);
+    document.getElementById("back_click_killer_window").remove();
+    document.getElementById("advanced_settings").remove();
+}
 
-    let title = document.createElement("h1");
-    title.innerHTML = light.name;
+function open_specific(light, index, xpos, ypos){
+    let backgroung_to_click_away = document.createElement("div");
+    backgroung_to_click_away.style.height = "98%";
+    backgroung_to_click_away.style.width = "98%";
+    backgroung_to_click_away.id = "back_click_killer_window";
+    backgroung_to_click_away.style.position = "absolute";
+
+    backgroung_to_click_away.addEventListener("click", remove_specific);
+
+    let advanced_settings_div = document.createElement("div");
+    advanced_settings_div.style.width = "500px";
+    advanced_settings_div.style.height = "400px";
+    advanced_settings_div.style.position = "absolute";
+    advanced_settings_div.style.top = ((ypos + 400 > window.innerHeight) ? ypos - 400 : ypos)+"px";
+    advanced_settings_div.style.left = ((xpos + 500 > window.innerWidth) ? xpos - 500 : xpos)+"px";
+    advanced_settings_div.style.border = "solid black 2px";
+    advanced_settings_div.style.backgroundColor = "#BDA69D"; 
+    advanced_settings_div.id = "advanced_settings";
 
     let brightness_slider = document.createElement("input");
     brightness_slider.type = "range";
@@ -62,6 +80,7 @@ function create_light_mode(light, index){
         xhr.open('PUT', encoded_url, true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send(["{\"bri\":" + this.value + "}"]);
+        light.state.bri = this.value;
     }
 
     let paragraph_bright = document.createElement("p");
@@ -81,6 +100,7 @@ function create_light_mode(light, index){
         xhr.open('PUT', encoded_url, true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send(["{\"hue\":" + this.value + "}"]);
+        light.state.hue = this.value;
     }
 
     let paragraph_hue = document.createElement("p");
@@ -100,6 +120,7 @@ function create_light_mode(light, index){
         xhr.open('PUT', encoded_url, true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send(["{\"sat\":" + this.value + "}"]);
+        light.state.sat = this.value;
     }
 
     let paragraph_sat = document.createElement("p");
@@ -107,29 +128,59 @@ function create_light_mode(light, index){
     paragraph_sat.style.marginBottom = 0;
     paragraph_sat.style.fontSize = "2em";
 
+    advanced_settings_div.appendChild(paragraph_bright);
+    advanced_settings_div.appendChild(brightness_slider);
+    advanced_settings_div.appendChild(paragraph_hue);
+    advanced_settings_div.appendChild(hue_slighter);
+    advanced_settings_div.appendChild(paragraph_sat);
+    advanced_settings_div.appendChild(sat_slighter);
+
+    document.getElementsByTagName("BODY")[0].appendChild(backgroung_to_click_away);
+    document.getElementsByTagName("BODY")[0].appendChild(advanced_settings_div);
+}
+
+function create_light_mode(light, index){
+    let container = document.createElement("div");
+    container.classList.add("containers");
+
+    let title = document.createElement("h1");
+    title.innerHTML = light.name;
+
+    let advance_button = document.createElement("div");
+    advance_button.innerHTML = "Advanced";
+    advance_button.classList.add("Advanced");
+    
+    advance_button.addEventListener("click", function(event) {
+        let xpos = event.clientX;
+        let ypos = event.clientY;
+        open_specific(light, index, xpos, ypos);
+    });
+
     let switch_obj = document.createElement("div");
     switch_obj.innerHTML = (light.state.on) ? "ON" : "OFF";
-    switch_obj.style.backgroundColor = (light.state.on) ? "green" : "red";
+    if(switch_obj.innerHTML == "ON"){
+        switch_obj.style.direction = "ltr";
+    } else {
+        switch_obj.style.direction = "rtl";
+    }
+    switch_obj.style.backgroundColor = (light.state.on) ? "#197F64" : "#99192F";
     switch_obj.classList.add("On_off_switch");
 
     switch_obj.addEventListener("click", function (){ 
         turn_off_light(light, index);
         if(switch_obj.innerHTML == "OFF"){
             switch_obj.innerHTML = "ON";
-            switch_obj.style.backgroundColor = "GREEN";
+            switch_obj.style.direction = "ltr";
+            switch_obj.style.backgroundColor = "#197F64";
         } else {
             switch_obj.innerHTML = "OFF";
-            switch_obj.style.backgroundColor = "RED";
+            switch_obj.style.direction = "rtl";
+            switch_obj.style.backgroundColor = "#99192F";
         }
     });
 
     container.appendChild(title);
-    container.appendChild(paragraph_bright);
-    container.appendChild(brightness_slider);
-    container.appendChild(paragraph_hue);
-    container.appendChild(hue_slighter);
-    container.appendChild(paragraph_sat);
-    container.appendChild(sat_slighter);
+    container.appendChild(advance_button);
     container.appendChild(switch_obj);
     document.getElementById("containter").appendChild(container);
 }
